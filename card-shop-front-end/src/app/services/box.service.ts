e23ef3ad11138e36d41b616f1ba9e22a76c7bfa8
willtreuel6 <willtreuel6@gmail.com>
2022-11-17 13:38:05 -0500

    added box service methods

--- card-shop-front-end/src/app/services/box.service.ts
+++ card-shop-front-end/src/app/services/box.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Box } from '../models/box';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoxService {
+  
+  
+  private baseUrl = "http://localhost:8081/api/box";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBoxList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  getBoxById(id : number): Observable<any>{
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  deleteBox(id : number) : Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType : 'text'})
+  }
+
+  createBox(box : Object): Observable<Object>{
+    return this.http.post(`${this.baseUrl}`, box)
+  }
+
+  updateBox(box : Object, id : number) : Observable<Object>{
+    return this.http.post(`${this.baseUrl}/${id}`, box)
+  }
 }
