d7da9538c23f257f11ee7edc0ceb9181c7af824a
willtreuel6 <willtreuel6@gmail.com>
2022-11-16 22:34:25 -0500

    project structure/files added

--- card-shop-front-end/src/app/app.module.ts
+++ card-shop-front-end/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    InventoryComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
