a2eec38d923e3092d87f0beaa5b7913f5c1a6ce7
willtreuel6 <willtreuel6@gmail.com>
2022-11-17 09:25:16 -0500

    Added Box Model

---
+++ card-shop-front-end/src/app/models/box.ts
@@ -0,0 +1,10 @@
+export interface Box{
+    boxId : number;
+    boxName : string;
+    boxBrand :  string;
+    boxYear : string;
+    boxSport : string;
+    boxPrice : string;
+    boxDescription : string;
+    inStock : boolean;
+}
