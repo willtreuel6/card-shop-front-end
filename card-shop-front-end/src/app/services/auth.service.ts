02591502c4b2341e006838ab8f00e2575a7fe83d
willtreuel6 <willtreuel6@gmail.com>
2022-11-29 15:35:32 -0500

    auth service crud ops done

--- card-shop-front-end/src/app/services/auth.service.ts
+++ card-shop-front-end/src/app/services/auth.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl : string = "http://localhost:8081/api/admin";
+
+  constructor(private http : HttpClient) { }
+
+  getAdminList(): Observable<object> {
+    return this.http.get(`${this.baseUrl}`);
+    
+  }
+
+  getAdminById(id : number): Observable<any>{
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  deleteAdmin(id : number) : Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType : 'text'})
+  }
+
+  createAdmin(box : Object): Observable<Object>{
+    return this.http.post(`${this.baseUrl}`, box)
+  }
+
+  updateAdmin( box : Object) : Observable<Object>{
+    return this.http.put(`${this.baseUrl}`, box)
+  }
+
+
 }
