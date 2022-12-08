70c7183bbfba34d5b15071c188d7f48d58b24581
willtreuel6 <willtreuel6@gmail.com>
2022-12-08 00:54:30 -0500

    email controls is dynamic and working with contact page

--- card-shop-front-end/src/app/components/admin-controls/admin-controls.component.ts
+++ card-shop-front-end/src/app/components/admin-controls/admin-controls.component.ts
@@ -1,26 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Contact } from 'src/app/models/contact';
 import { AdminControlsService } from 'src/app/services/admin-controls.service';
 
 @Component({
   selector: 'app-admin-controls',
   templateUrl: './admin-controls.component.html',
-  styleUrls: ['./admin-controls.component.css']
+  styleUrls: ['./admin-controls.component.css'],
+  providers: [ConfirmationService, MessageService]
 })
 export class AdminControlsComponent implements OnInit {
 
-  contactInfo : any = {};
-  constructor(private adminControlsService : AdminControlsService) { }
+  contactInfoSet : any = {};
+
+  contactInfo : Contact;
+
+  contactInfoDialog : boolean = false;
+
+  constructor(private adminControlsService : AdminControlsService, private confirmationService : ConfirmationService) { }
 
   ngOnInit(): void {
-    this.adminControlsService.getContact()
-    .subscribe(res => {
-      console.log(res)
-      this.contactInfo = res
-    });
+    this.reloadData();
 
   }
 
-  editContact(contactInfo : object){}
+  reloadData(){
+    this.adminControlsService.getContact()
+    .subscribe(res => {
+      console.log(res)
+      this.contactInfoSet = res
+    });
+  }
+
+
+  editContact(contactInfo : object){
+    this.contactInfoDialog = true;
+    this.contactInfo = {...contactInfo};
+    console.log(contactInfo);
+  }
+
+  hideDialog(){
+    this.contactInfoDialog = false;
+
+  }
+
+  saveContactInfo(info : any){
+    console.log(info);
+    this.confirmationService.confirm({
+      message: "Are you sure you want to change contact page?",
+      header: "Confirm",
+      icon: "pi pi-exclamation-triangle",
+      accept: ()  => {
+        this.adminControlsService.updateContact(info).subscribe(data => {
+          console.log("inside")
+          console.log(data);
+          this.reloadData();
+        });
+      }
+    })
+    this.contactInfoDialog = false;
+  }
 
 }
--- card-shop-front-end/src/app/components/contact/contact.component.ts
+++ card-shop-front-end/src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminControlsService } from 'src/app/services/admin-controls.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactInfoSet : any = [];
+
+  constructor(private adminControlsService : AdminControlsService) { }
 
   ngOnInit(): void {
+    this.reloadData();
+
   }
 
+  reloadData(){
+    this.adminControlsService.getContact()
+    .subscribe(res => {
+      console.log(res)
+      this.contactInfoSet = res
+    });
+
+  }
 }
