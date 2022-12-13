a6c6b58e651a47060661a294a03995abb241c3e1
willtreuel6 <willtreuel6@gmail.com>
2022-12-12 22:29:29 -0500

    card inventory.ts file stuff to make dataview work

--- card-shop-front-end/src/app/components/card-inventory/card-inventory.component.ts
+++ card-shop-front-end/src/app/components/card-inventory/card-inventory.component.ts
@@ -1,31 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { Card } from 'src/app/models/card';
 import { CardService } from 'src/app/services/card.service';
+import {SelectItem} from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-card-inventory',
   templateUrl: './card-inventory.component.html',
-  styleUrls: ['./card-inventory.component.css']
+  styleUrls: ['./card-inventory.component.scss']
 })
 export class CardInventoryComponent implements OnInit {
 
+  sports: any[];
 
-  card : Card;
+  selectedSports : any[];
 
-  cards : any = {};
+  //card : Card;
 
-  constructor(private cardService : CardService) { }
+  cards : any = [];
+
+  sortOptions: SelectItem[];
+
+  sortOrder: number;
+
+  sortField: string;
+
+  constructor(private cardService : CardService, private primengConfig : PrimeNGConfig) {
+   }
 
   ngOnInit(): void {
     this.reloadData();
+
+    this.sortOptions = [
+      {label : 'Price High to Low', value: '!price'},
+      {label: 'Price Low to High', value: 'price'}
+    ];
+
+    this.primengConfig.ripple = true;
   }
 
   reloadData(){
     this.cardService.getCardList()
     .subscribe(res => {
-      this.cards = res
-    });
-    console.log(this.cards);
+      this.cards = res;
+      console.log(res)
+      console.log(this.cards)
+    })
+  }
+
+  onSortChange(event: { value: any; }){
+    let value = event.value;
+
+    if(value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    }else{
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
   }
 
 }
