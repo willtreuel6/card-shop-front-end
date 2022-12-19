b0e31cedd527f23e05a30232f70fcad376f9f5fd
willtreuel6 <willtreuel6@gmail.com>
2022-12-19 10:14:12 -0500

    added faq model ts file

---
+++ card-shop-front-end/src/app/models/faq.ts
@@ -0,0 +1,15 @@
+export interface Faq{
+    faqId? : number;
+    faqBoldHeader? : string;
+    faqDescription? : string;
+    faqQOne? :  string;
+    faqQTwo? : string;
+    faqQThree? : string;
+    faqQFour? : string;
+    faqAOne? : string;
+    faqATwo? : string;
+    faqAThree? : string;
+    faqAFour? : string;
+    faqBoldFoot : string;
+    faqFootDescription : string;
+}
